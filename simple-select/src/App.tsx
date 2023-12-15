ab9ee2d87bbd5e9180d63cf99ae229ab86c85666
[AI9] 허제인 <codingjamee@gmail.com>
2023-12-15 21:42:48 +0900

    Feat: 선택하거나 마우스엔터하면 스타일링 변하는기능
    한개 선택되는 기능은 완료
    여러개 선택하여 버튼으로 보여주는 기능 구현중

--- simple-select/src/App.tsx
+++ simple-select/src/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import Select from "./Select";
+import { useEffect, useState } from "react";
+import Select, { SelectOptions } from "./Select";
 
 const options = [
   { label: "First", value: 1 },
@@ -7,15 +7,24 @@ const options = [
   { label: "Third", value: 3 },
   { label: "Fourth", value: 4 },
   { label: "Fifth", value: 5 },
-  { label: "sixth", value: 6 },
+  { label: "Sixth", value: 6 },
 ];
 
 const App = () => {
-  const [value, setValue] = useState<(typeof options)[0] | undefined>(
-    options[0]
-  );
+  const [value1, setValue1] = useState<SelectOptions[]>([options[0]]);
+  const [value2, setValue2] = useState<SelectOptions | undefined>(options[0]);
+
   return (
-    <Select options={options} value={value} onChange={(e) => setValue(e)} />
+    <>
+      <Select
+        multiple
+        options={options}
+        value={value1}
+        onChange={(o) => setValue1(o)}
+      />
+      <br />
+      <Select options={options} value={value2} onChange={(o) => setValue2(o)} />
+    </>
   );
 };
 export default App;
--- simple-select/src/Select.tsx
+++ simple-select/src/Select.tsx
@@ -1,26 +1,119 @@
+import { useEffect, useState } from "react";
 import styles from "./select.module.css";
 
-type SelectOptions = {
+export type SelectOptions = {
   label: string;
-  value: string | number | boolean;
+  value: string | number;
+};
+
+type MultipleSelectProps = {
+  multiple: true;
+  value: SelectOptions[];
+  onChange: (value: SelectOptions[]) => void;
+};
+
+type SingleSelectProps = {
+  multiple?: false;
+  value?: SelectOptions;
+  onChange: (value: SelectOptions | undefined) => void;
 };
 
 type SelectProps = {
   options: SelectOptions[];
-  value?: SelectOptions | undefined;
-  onChange: (value: SelectOptions | undefined) => void;
-};
+} & (SingleSelectProps | MultipleSelectProps);
 
-const Select = ({ options, value, onChange }: SelectProps) => {
+const Select = ({ multiple, options, value, onChange }: SelectProps) => {
+  const [isOpen, setIsopen] = useState(false);
+  const [highlightedIndex, setHighlightedIndex] = useState(0);
+
+  const clearOptions = () => {
+    multiple ? onChange([]) : onChange(undefined);
+  };
+
+  //해당 li를 선택하면
+  //멀티플이면 모두 value에 넣어야 함
+
+  //li가 선택되어 있으면 언셀렉트 해야함
+
+  // value.includes is not a function
+  const selectOption = (option: SelectOptions) => {
+    if (multiple) {
+      console.log(value);
+      if (value.includes(option)) {
+        onChange(value.filter((o) => o !== option));
+      } else {
+        onChange([...value, option]);
+      }
+    } else {
+      if (option !== value) {
+        onChange(option);
+      }
+    }
+  };
+
+  const isOptionSelected = (option: SelectOptions) => {
+    return multiple ? value.includes(option) : value === option;
+  };
+
+  useEffect(() => {
+    isOpen && setHighlightedIndex(0);
+  }, [isOpen]);
+
   return (
-    <div tabIndex={0} className={styles.container}>
-      <span className={styles.value}>Value</span>
-      <button className={styles["close-btn"]}>&times;</button>
+    <div
+      tabIndex={0}
+      className={styles.container}
+      onClick={() => {
+        setIsopen((prev) => !prev);
+      }}
+      onBlur={() => {
+        setIsopen(false);
+      }}
+    >
+      <span className={styles.value}>
+        {multiple
+          ? value.map((v) => (
+              <button
+                key={v.value}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  selectOption(v);
+                }}
+                className={styles["option-badge"]}
+              >
+                {v.label}
+              </button>
+            ))
+          : value?.label}
+      </span>
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          clearOptions();
+        }}
+        className={styles["close-btn"]}
+      >
+        &times;
+      </button>
       <div className={styles.divider}></div>
       <div className={styles.caret}></div>
-      <ul className={`${styles.options} ${styles.show}`}>
-        {options.map((option) => (
-          <li key={`option-${option.value}`} className={styles.option}>
+      <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
+        {options.map((option, index) => (
+          <li
+            key={option.value}
+            onClick={(e) => {
+              e.stopPropagation();
+              selectOption(option);
+              setIsopen(false);
+            }}
+            onMouseEnter={(e) => {
+              e.stopPropagation();
+              setHighlightedIndex(index);
+            }}
+            className={`${styles.option} ${
+              isOptionSelected(option) ? styles.selected : ""
+            } ${index === highlightedIndex ? styles.highlighted : ""}`}
+          >
             {option.label}
           </li>
         ))}
