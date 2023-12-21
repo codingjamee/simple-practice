ac5a1c9841a6b4e3573867448244e263cc7d5386
허제인 <codingjamee@gmail.com>
2023-12-21 15:42:08 +0900

    Design: 스타일링 수정

--- dropdown_two/src/App.tsx
+++ dropdown_two/src/App.tsx
@@ -6,14 +6,20 @@ function App() {
   // const onChange = () => {};
   return (
     <Dropdown>
-      <DropdownButton>d-button</DropdownButton>
+      <DropdownButton>d-오예!@</DropdownButton>
       <DropdownItem id="id-1" value="1">
         aasasd
       </DropdownItem>
-      <DropdownItem id="id-2" value="2"></DropdownItem>
-      {/* <DropdownItem></DropdownItem>
-      <DropdownItem></DropdownItem>
-      <DropdownItem></DropdownItem> */}
+      <DropdownItem id="id-2" value="2">
+        bbbadsf
+      </DropdownItem>
+      <DropdownItem id="id-3" value="3">
+        ewefw
+      </DropdownItem>
+      <DropdownItem id="id-4" value="4">
+        dfsafwefg
+      </DropdownItem>
+      {/* <DropdownItem></DropdownItem> */}
     </Dropdown>
   );
 }
--- dropdown_two/src/styles/Dropdown.style.ts
+++ dropdown_two/src/styles/Dropdown.style.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Container = styled.div`
   position: relative;
@@ -15,50 +15,21 @@ export const Container = styled.div`
     border-color: hsl(200, 100%, 50%);
   }
 `;
-export const Button = styled.div`
-  flex-grow: 1;
-`;
 
 export const Value = styled.span`
   flex-grow: 1;
 `;
-// /* flex-grow 최대한 커짐 */
-// .value {
-// }
-
-// .close-btn {
-//   background: none;
-//   color: #777;
-//   border: none;
-//   outline: none;
-//   cursor: pointer;
-//   padding: 0;
-//   font-size: 1.25em;
-// }
 
 // .close-btn:focus,
 // .close-btn:hover {
 //   color: #333;
 // }
 
-// /* height값이 부모에 없기 때문에
-// 100%가 뭔지 모름!
-// */
-// .divider {
-//   background-color: #777;
-//   align-self: stretch;
-//   /* flex를 썼기 떄문에 해당 공간을 모두 차지하게 보여주는 코드 */
-//   width: 0.05em;
-// }
+interface OptionsProps {
+  isOpen: boolean;
+}
 
-// /* 테두리 윗면의 색상을 주어 캐럿모양 표현 */
-// .caret {
-//   translate: 0 25%;
-//   border: 0.25em solid transparent;
-//   border-top-color: #777;
-// }
-
-export const Options = styled.div`
+export const Options = styled.div<OptionsProps>`
   margin: 0;
   padding: 0;
   list-style: none;
@@ -74,16 +45,44 @@ export const Options = styled.div`
   background-color: white;
   z-index: 100;
   display: block;
+  padding: 0.25em 0.5em;
+  cursor: pointer;
+  ${(props) =>
+    !props.isOpen &&
+    css`
+      background: none;
+      color: #777;
+      border: none;
+      outline: none;
+      cursor: pointer;
+      padding: 0;
+      font-size: 1.25em;
+    `}
+`;
+export const Close = styled.button`
+  background: none;
+  color: #777;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 0;
+  font-size: 1.25em;
 `;
 
-// .options.show {
-//   display: block;
-// }
+// /* height값이 부모에 없기 때문에
+// 100%가 뭔지 모름!
+// */
+export const Divider = styled.div`
+  background-color: #777;
+  align-self: stretch;
+  width: 0.05em;
+`;
 
-// .option {
-//   padding: 0.25em 0.5em;
-//   cursor: pointer;
-// }
+export const Caret = styled.div`
+  translate: 0 25%;
+  border: 0.25em solid transparent;
+  border-top-color: #777;
+`;
 
 // .option.selected {
 //   background-color: hsl(200, 100%, 70%);
