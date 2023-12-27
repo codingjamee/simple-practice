03ef742cd443c8499d908fd86b6184065caa3e83
허제인 <codingjamee@gmail.com>
2023-12-27 17:47:23 +0900

    Feat: logRoles 사용해보기

--- webpack-practice-two/src/App.test.jsx
+++ webpack-practice-two/src/App.test.jsx
@@ -1,12 +1,15 @@
 /**
  * @jest-environment jsdom
  */
+import { logRoles } from "@testing-library/dom";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("button starts with correct color", () => {
-  render(<App />);
+  const { container } = render(<App />);
+  logRoles(container);
+
   const buttonElement = screen.getByRole("button", { name: /blue/i });
   // getByRole이 요소를 찾지 못하면 에러! 테스트 실패
   // name은 버튼에 표시되는 문자열
