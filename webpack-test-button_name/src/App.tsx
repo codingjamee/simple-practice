d0ccadf1d4acbf0a52ef757faa7e59cc9eb12f8d
허제인 <codingjamee@gmail.com>
2023-12-27 19:49:35 +0900

    File: 폴더명 변경,
    테스팅을 위해 @testing-library/dom설치
    css 적용을 위해 css-loader, style-loader설치
    디버깅을 위해 @babel/core, react 설치해봄

--- webpack-test-button_name/src/App.tsx
+++ webpack-test-button_name/src/App.tsx
@@ -1,18 +1,19 @@
-import { useState } from "react";
 import "./styles.css";
+import React, { useState } from "react";
 
 const App = () => {
   const [classes, setClasses] = useState("red");
-  const [innerTxt, setInnerTxt] = useState("blue");
+  const [innerTxt, setInnerTxt] = useState("red");
   const onClickBtn = () => {
     setClasses((prev) => (prev === "red" ? "blue" : "red"));
-    setInnerTxt((prev) => (prev === "Hi" ? "Bye" : "Hi"));
+    setInnerTxt((prev) => (prev === "red" ? "blue" : "red"));
   };
   return (
     <button className={classes} onClick={onClickBtn}>
       {innerTxt}
     </button>
   );
+  // return <div>hello webpack</div>;
 };
 
 export default App;
