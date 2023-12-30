5fe01c2797b43b8e4a93197caf64b96e166bfb45
허제인 <codingjamee@gmail.com>
2023-12-30 13:49:43 +0900

    Fix: App 컴포넌트 사용자 사용예시

--- carousel/src/App.tsx
+++ carousel/src/App.tsx
@@ -2,20 +2,40 @@ import React from "react";
 import Carousel from "./component/Carousel";
 import CarouselItem from "./component/CarouselItem";
 import TemplateCard from "./component/TemplateCard";
+import a from "./assets/a.png";
+import b from "./assets/b.png";
+import c from "./assets/c.png";
 
 const App = () => {
   return (
-    <Carousel>
-      <CarouselItem>
-        <TemplateCard />
-      </CarouselItem>
-      <CarouselItem>
-        <TemplateCard />
-      </CarouselItem>
-      <CarouselItem>
-        <TemplateCard />
-      </CarouselItem>
-    </Carousel>
+    <>
+      <Carousel>
+        <CarouselItem>
+          <TemplateCard>
+            <img src={a} alt="a-img" />
+            <img src={b} alt="b-img" />
+            <img src={c} alt="c-img" />
+            <p>one</p>
+          </TemplateCard>
+        </CarouselItem>
+        <CarouselItem>
+          <TemplateCard>
+            <img src={b} alt="b-img" />
+            <img src={a} alt="a-img" />
+            <img src={c} alt="c-img" />
+            <p>two</p>
+          </TemplateCard>
+        </CarouselItem>
+        <CarouselItem>
+          <TemplateCard>
+            <img src={c} alt="c-img" />
+            <img src={a} alt="a-img" />
+            <img src={b} alt="b-img" />
+            <p>three</p>
+          </TemplateCard>
+        </CarouselItem>
+      </Carousel>
+    </>
   );
 };
 
