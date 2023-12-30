fc467e54cfd72671f790ecfbf91426d91580246a
허제인 <codingjamee@gmail.com>
2023-12-31 01:05:34 +0900

    Style: 주석삭제

--- carousel/src/component/CarouselItem.tsx
+++ carousel/src/component/CarouselItem.tsx
@@ -5,16 +5,7 @@ type CarouselItemPropsType = {
 };
 
 const CarouselItem = (props: CarouselItemPropsType) => {
-  //현재 보이고 있는 아이템이 되려면 selected를 viewed로?
-  // {flattenChildren(props.children).map(child => {
-  //   if (child.type === MenuItem) {
-  //     return React.cloneElement(child, {
-  //       selected: child.key === selectedKey,
-  //       onClick: () => setSelectedKey(child.key)
-  //   });
-  // }
-
-  return <div>{props.children}</div>;
+  return <div style={{ width: "100%" }}>{props.children}</div>;
 };
 
 export default CarouselItem;
